f8310badc0c1c9c3a22505285af3f7d7778a9d6a
Jin <jin.han@lambda256.io>
2019-10-25 20:59:26 +0900

    add token solidity file with feeCollector

---
+++ contracts/fee/RupiahFeeCollector.sol
@@ -0,0 +1,286 @@
+pragma solidity 0.4.25;
+
+import "../math/SafeMath.sol";
+import "./IFeeCollector.sol";
+import "../ownership/Ownable.sol";
+import "../token/ERC20RupiahTokenL.sol";
+
+contract RupiahFeeCollector is IFeeCollector, Ownable {
+    using SafeMath for uint256;
+
+    event DistributeFee(address indexed _collector, uint256 _value);
+    event Withdraw(address indexed _from, uint256 value);
+
+    // 토큰 수수료 문자와 분모
+    uint256 _feeRatioNumeratorFromToken;
+    uint256 _feeRatioDenominatorFromToken;
+
+    // 수수료 가져갈 목록 //TODO: struct로 합치는게 좋을듯
+    address[] _feeCollectors;
+    // 각각이 가져가는 수수료 비율
+    mapping(address => uint256) _feeRatio;
+    mapping(address => uint256) _deposit;
+    // 각각 가져갈 수수료율을 계산하기 위해서 나눠줄 분모 (소수점이 안되니깐)
+    uint256 _feeDenominatorInCollectors;
+
+    // whitelist
+    mapping(address => bool) _fromWhitelist;
+    mapping(address => bool) _toWhitelist;
+
+    uint8 SENDER = 0;
+    uint8 RECEIVER = 1;
+
+    ERC20RupiahTokenL rupiahToken;
+
+    modifier onlyCollectors () {
+        require(isCollector());
+        _;
+    }
+
+    function isCollector() public view returns (bool) {
+        return _feeRatio[msg.sender] > 0;
+    }
+
+    modifier onlyRupiahToken () {
+        require(isRupiahToken());
+        _;
+    }
+
+    function isRupiahToken() public view returns (bool) {
+        return msg.sender == address(rupiahToken);
+    }
+
+    /**
+      address[] _initFromWhitelistEntries `from` 화이트리스트 주소 목록
+      address[] _initToWhitelistEntries `to` 화이트리스트 주소 목록
+      address[] _initFeeCollectorEntries FeeCollector의 주소 목록  !! 아래 비율과 순서가 맞아야 함 !!
+      uint256[] _initFeeRatioInCollectorsEntries 각 FeeCollecor 간 fee 비율 ex 10, 20, 30
+      address _initIDRTContractAddress RupiahToken 컨트랙트 주소
+      uint256 _initFeeRatioNumerator 전체 수수료 비율의 분자   예) 1.5%를 구현하고 싶으면 => 15
+      uint256 _initFeeRatioDenominator 전체 수수료 비율의 분모 예) 1.5%를 구현하고 싶으면 => 1000
+    */
+    constructor(address[] _initFromWhitelistEntries, address[] _initToWhitelistEntries,
+        address[] _initFeeCollectorEntries, uint256[] _initFeeRatioInCollectorsEntries,
+        address _initIDRTContractAddress, uint256 _initFeeRatioNumerator, uint256 _initFeeRatioDenominator) public {
+        require(_initFeeRatioDenominator >= _initFeeRatioNumerator, "Denominator must be bigger than numerator");
+
+        _addWhitelistBulk(_initFromWhitelistEntries, SENDER);
+        _addWhitelistBulk(_initToWhitelistEntries, RECEIVER);
+
+        _initMapRatioWithCollector(_initFeeCollectorEntries, _initFeeRatioInCollectorsEntries);
+
+        rupiahToken = ERC20RupiahTokenL(_initIDRTContractAddress);
+
+        _feeRatioNumeratorFromToken = _initFeeRatioNumerator;
+        _feeRatioDenominatorFromToken = _initFeeRatioDenominator;
+
+    }
+
+    function _initMapRatioWithCollector(address[]  _initFeeCollectorEntries, uint256[]  _initFeeRatioEntries) private {
+        require(_initFeeCollectorEntries.length == _initFeeRatioEntries.length);
+        uint256 sum = 0;
+
+        _feeCollectors = _initFeeCollectorEntries;
+
+        for (uint16 i = 0; i < _initFeeRatioEntries.length; i++) {
+            uint256 ratio = _initFeeRatioEntries[i];
+            address addr = _initFeeCollectorEntries[i];
+            _feeRatio[addr] = ratio;
+            sum = sum.add(ratio);
+        }
+
+        _feeDenominatorInCollectors = sum;
+    }
+    function calcFee(address _from, address _to, uint256 _value) public onlyRupiahToken returns (uint256) {
+        if (isWhitelist(_from, SENDER) || isWhitelist(_to, RECEIVER) || _from == address(this)) {
+            return 0;
+        }
+        return _calcFee(_value); // 최종 수수료 리턴
+    }
+
+    function _calcFee(uint256 _value) private returns (uint256){
+        uint256 fee = _value.mul(_feeRatioNumeratorFromToken).div(_feeRatioDenominatorFromToken);
+        uint256 sum = fee;
+        for (uint256 i = 0; i < _feeCollectors.length; i++) {
+            address collector = _feeCollectors[i];
+            uint256 feeRatio = _feeRatio[collector];
+            uint256 f = fee.mul(feeRatio).div(_feeDenominatorInCollectors);
+
+            if (i == _feeCollectors.length - 1){
+                f = sum;
+            }
+            else {
+                sum = sum.sub(f);
+            }
+
+            _deposit[collector] = _deposit[collector].add(f);
+            emit DistributeFee(collector, f);
+        }
+
+        return fee;
+    }
+
+    function withdraw() external onlyCollectors returns (uint256) {
+        return _withdraw(msg.sender);
+    }
+
+    function withdrawAll() external onlyOwner returns (uint256) {
+        uint256 sum = 0;
+        for (uint16 i = 0; i < _feeCollectors.length; i++) {
+            sum = sum.add(_withdraw(_feeCollectors[i]));
+        }
+        // 수수료 다 전달
+        return sum;
+    }
+
+    function _withdraw(address _to) private returns (uint256){
+        uint256 balance = _deposit[_to];
+
+        rupiahToken.transfer(_to, balance);
+        _deposit[_to] = 0;
+
+        emit Withdraw(_to, balance);
+        return balance;
+    }
+    function isWhitelist(address addr, uint8 whitelistType) public view returns (bool) {
+        require(whitelistType == SENDER|| whitelistType == RECEIVER);
+
+        if (whitelistType == SENDER) {
+            return _fromWhitelist[addr];
+        }
+        else if(whitelistType == RECEIVER) {
+            return _toWhitelist[addr];
+        }
+        return false;
+    }
+
+    function addWhitelist(address addr, uint8 whitelistType) external onlyOwner returns (bool) {
+        return _addWhitelist(addr, whitelistType);
+    }
+
+    function _addWhitelist(address addr, uint8 whitelistType) private onlyOwner returns (bool) {
+        require(whitelistType == SENDER || whitelistType == RECEIVER);
+        require(isWhitelist(addr, whitelistType) == false);
+
+         if (whitelistType == SENDER) {
+             _fromWhitelist[addr] = true;
+         }
+        else if(whitelistType == RECEIVER) {
+             _toWhitelist[addr] = true;
+         }
+        else {
+             return false;
+         }
+
+        return true;
+    }
+
+    function addWhitelistBulk(address[] addrEntries, uint8 whitelistType) external onlyOwner returns (bool) {
+        require(whitelistType == SENDER || whitelistType == RECEIVER);
+
+        return _addWhitelistBulk(addrEntries, whitelistType);
+    }
+
+    function _addWhitelistBulk(address[] addrEntries, uint8 whitelistType) private onlyOwner returns (bool) {
+        require(whitelistType == SENDER || whitelistType == RECEIVER);
+
+        for (uint16 i = 0; i < addrEntries.length; i++) {
+            address addr = addrEntries[i];
+            if (addr == address(0)){
+                revert();
+            }
+            _addWhitelist(addr, whitelistType);
+        }
+        return true;
+    }
+    function deleteWhitelist(address addr, uint8 whitelistType) external onlyOwner returns (bool) {
+        require(whitelistType == SENDER || whitelistType == RECEIVER);
+        return _deleteWhitelist(addr, whitelistType);
+    }
+
+    function _deleteWhitelist(address addr, uint8 whitelistType) private returns (bool) {
+        require(whitelistType == SENDER || whitelistType == RECEIVER);
+        require(isWhitelist(addr, whitelistType) == true);
+
+        if (whitelistType == RECEIVER) {
+            _toWhitelist[addr] = false;
+        }
+        else {
+            _fromWhitelist[addr] = false;
+        }
+
+        return true;
+    }
+
+    function deleteWhitelistBulk(address[] addrEntries, uint8 whitelistType) external onlyOwner returns (bool) {
+        require(whitelistType == SENDER || whitelistType == RECEIVER);
+
+        for (uint16 i = 0; i < addrEntries.length; i++) {
+            address addr = addrEntries[i];
+            if (addr == address(0)){
+                revert();
+            }
+            _deleteWhitelist(addr, whitelistType);
+        }
+        return true;
+    }
+
+    function addFeeWithCollector(address addr, uint256 fee) onlyOwner external returns (bool) {
+        return _addFeeWithCollector(addr, fee);
+    }
+    function _addFeeWithCollector(address addr, uint256 fee) private returns (bool) {
+        require(addr != address(0));
+        require(fee > 0);
+        require(_feeRatio[addr] == 0, "already added account");
+
+        _feeCollectors.push(addr);
+        _feeRatio[addr] = fee;
+        _feeDenominatorInCollectors = _feeDenominatorInCollectors.add(fee);
+
+        return true;
+    }
+    function deleteFeeWithCollector(address addr) onlyOwner external returns (bool){
+        return _deleteFeeWithCollector(addr);
+    }
+    function _deleteFeeWithCollector(address addr) private returns (bool) {
+        require(addr != address(0));
+
+        bool found = false;
+        uint256 i;
+        for (i = 0; i < _feeCollectors.length; i++) {
+            if (_feeCollectors[i] == addr) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found == true) {
+            uint256 fee = _feeRatio[addr];
+            _feeDenominatorInCollectors = _feeDenominatorInCollectors.sub(fee);
+            delete _feeRatio[addr];
+            remove(i);
+            return true;
+        }
+        return false;
+    }
+    function remove(uint256 index) internal {
+        if (index >= _feeCollectors.length) return;
+
+        for (uint i = index; i < _feeCollectors.length - 1; i++){
+            _feeCollectors[i] = _feeCollectors[i + 1];
+        }
+        delete _feeCollectors[_feeCollectors.length-1];
+        _feeCollectors.length--;
+    }
+    function setFeeRatio(uint256 numerator, uint256 denominator) onlyOwner external returns (bool) {
+        require(denominator >= numerator, "Denominator must be bigger than numerator");
+        _feeRatioNumeratorFromToken = numerator;
+        _feeRatioDenominatorFromToken = denominator;
+        return true;
+    }
+
+    function getFeeRatio() onlyOwner external view returns (uint256[2] memory) {
+        return [_feeRatioNumeratorFromToken, _feeRatioDenominatorFromToken];
+    }
+
+}
---
+++ contracts/token/ERC20RupiahTokenL.sol
@@ -0,0 +1,310 @@
+/**
+ * Rupiah Token Smart Contract
+ * Copyright (C) 2019 PT. Rupiah Token Indonesia <https://www.rupiahtoken.com/>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This file incorporates work covered byt the following copyright and
+ * permission notice:
+ *
+ *     OpenZeppelin <https://github.com/OpenZeppelin/openzeppelin-solidity/>
+ *     Copyright (c) 2016 Smart Contract Solutions, Inc.
+ *     Modified for Rupiah Token by FengkieJ 2019.
+ *
+ *     centre-tokens <https://github.com/centrehq/centre-tokens>
+ *     Copyright CENTRE SECZ 2018.
+ *     Modified for Rupiah Token by FengkieJ 2019.
+ *
+ *     ZeppelinOS (zos) <https://github.com/zeppelinos/zos>
+ *     Copyright (c) 2018 ZeppelinOS Global Limited.
+ *
+ *     The MIT License (MIT)
+ *
+ *     Permission is hereby granted, free of charge, to any person obtaining a copy
+ *     of this software and associated documentation files (the "Software"), to deal
+ *     in the Software without restriction, including without limitation the rights
+ *     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *     copies of the Software, and to permit persons to whom the Software is furnished to
+ *     do so, subject to the following conditions:
+ *
+ *     The above copyright notice and this permission notice shall be included in all
+ *     copies or substantial portions of the Software.
+ *
+ *     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *     WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ *     CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+pragma solidity 0.4.25;
+
+import "./IERC20.sol";
+import "../math/SafeMath.sol";
+import "../governance/Blacklistable.sol";
+import "../lifecycle/Pausable.sol";
+import "../ownership/Ownable.sol";
+import "../zos/Initializable.sol";
+import "../fee/IFeeCollector.sol";
+
+/**
+ * @title ERC20RupiahTokenL
+ * @dev ERC20 compliant fiat token that is backed by Indonesian Rupiah 1:1
+ */
+contract ERC20RupiahTokenL is IERC20, Blacklistable, Initializable {
+    using SafeMath for uint256;
+
+    event Mint(address indexed beneficiary, uint256 amount);
+    event Burn(address indexed beneficiary, uint256 amount);
+    event TransferWithFee(address indexed from, address indexed to, uint256 amount, address indexed feeCollector, uint256 fee);
+
+    IFeeCollector RupiahFeeCollector;
+    string internal _name;
+    string internal _symbol;
+    string internal _currency;
+    uint8 internal _decimals;
+
+    mapping (address => uint256) internal _balances;
+    mapping (address => mapping (address => uint256)) internal _allowed;
+    uint256 internal _totalSupply;
+
+    /**
+     * @dev Initialize the smart contract to work with ZeppelinOS, can only be called once.
+     * @param name describes the name of the token.
+     * @param symbol describes the symbol of the token.
+     * @param currency describes the currency of the token.
+     * @param decimals describes the number of decimals of the token.
+     */
+    function initialize(string name, string symbol, string currency, uint8 decimals) initializer public {
+	owner = msg.sender;
+        _name = name;
+        _symbol = symbol;
+        _currency = currency;
+        _decimals = decimals;
+    }
+
+    /**
+     * @return the name of the token.
+     */
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @return the symbol of the token.
+     */
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * @return the currency of the token.
+     */
+    function currency() public view returns (string memory) {
+        return _currency;
+    }
+
+    /**
+     * @return the number of decimals of the token.
+     */
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+
+    /**
+     * @return the total number of tokens in existence
+     */
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+    * @dev Gets the balance of the specified address.
+    * @param owner The address to query the balance of.
+    * @return An uint256 representing the amount owned by the passed address.
+    */
+    function balanceOf(address owner) public view returns (uint256) {
+        return _balances[owner];
+    }
+
+    /**
+     * @dev Function to check the amount of tokens that an owner allowed to a spender.
+     * @param owner address The address which owns the funds.
+     * @param spender address The address which will spend the funds.
+     * @return A uint256 specifying the amount of tokens still available for the spender.
+     */
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return _allowed[owner][spender];
+    }
+
+    /**
+    * @dev Transfer token for a specified address
+    * @param to The address to transfer to.
+    * @param value The amount to be transferred.
+    */
+    function transfer(address to, uint256 value) public whenNotPaused notBlacklisted(msg.sender) notBlacklisted(to) returns (bool) {
+        _transferWithFee(msg.sender, to, value, RupiahFeeCollector);
+        return true;
+    }
+
+    /**
+     * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+     * Beware that changing an allowance with this method brings the risk that someone may use both the old
+     * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+     * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     * @param spender The address which will spend the funds.
+     * @param value The amount of tokens to be spent.
+     */
+    function approve(address spender, uint256 value) public whenNotPaused notBlacklisted(msg.sender) notBlacklisted(spender) returns (bool) {
+        _approve(msg.sender, spender, value);
+        return true;
+    }
+
+    /**
+     * @dev Transfer tokens from one address to another.
+     * Note that while this function emits an Approval event, this is not required as per the specification,
+     * and other compliant implementations may not emit the event.
+     * @param from address The address which you want to send tokens from
+     * @param to address The address which you want to transfer to
+     * @param value uint256 the amount of tokens to be transferred
+     */
+    function transferFrom(address from, address to, uint256 value) public whenNotPaused notBlacklisted(msg.sender) notBlacklisted(from) notBlacklisted(to) returns (bool) {
+        _approve(from, msg.sender, _allowed[from][msg.sender].sub(value));
+        _transferWithFee(from, to, value, RupiahFeeCollector);
+        return true;
+    }
+
+    function transferBridge(address to, uint256 value, address bridge) public returns (bool) {
+        _transferWithFee(msg.sender, to, value, bridge);
+        return true;
+    }
+
+    /**
+     * @dev Increase the amount of tokens that an owner allowed to a spender.
+     * approve should be called when allowed_[_spender] == 0. To increment
+     * allowed value is better to use this function to avoid 2 calls (and wait until
+     * the first transaction is mined)
+     * From MonolithDAO Token.sol
+     * Emits an Approval event.
+     * @param spender The address which will spend the funds.
+     * @param addedValue The amount of tokens to increase the allowance by.
+     */
+    function increaseAllowance(address spender, uint256 addedValue) public whenNotPaused notBlacklisted(msg.sender) notBlacklisted(spender) returns (bool) {
+        _approve(msg.sender, spender, _allowed[msg.sender][spender].add(addedValue));
+        return true;
+    }
+
+    /**
+     * @dev Decrease the amount of tokens that an owner allowed to a spender.
+     * approve should be called when allowed_[_spender] == 0. To decrement
+     * allowed value is better to use this function to avoid 2 calls (and wait until
+     * the first transaction is mined)
+     * From MonolithDAO Token.sol
+     * Emits an Approval event.
+     * @param spender The address which will spend the funds.
+     * @param subtractedValue The amount of tokens to decrease the allowance by.
+     */
+    function decreaseAllowance(address spender, uint256 subtractedValue) public whenNotPaused notBlacklisted(msg.sender) notBlacklisted(spender) returns (bool) {
+        _approve(msg.sender, spender, _allowed[msg.sender][spender].sub(subtractedValue));
+        return true;
+    }
+
+    /**
+     * @dev Function that mints an amount of the token and assigns it to
+     * an account. This encapsulates the modification of balances such that the
+     * proper events are emitted.
+     * @param account The account that will receive the created tokens.
+     * @param value The amount that will be created.
+     */
+    function mint(address account, uint256 value) public whenNotPaused notBlacklisted(account) onlyOwner {
+        require(account != address(0));
+
+        value = value.mul(10 ** uint256(_decimals));
+        _totalSupply = _totalSupply.add(value);
+        _balances[account] = _balances[account].add(value);
+        emit Transfer(address(0), account, value);
+        emit Mint(account, value);
+
+    }
+
+    /**
+     * @dev Function that burns an amount of the token.
+     * @param value The amount that will be burnt.
+     */
+    function burn(uint256 value) public whenNotPaused onlyOwner {
+        value = value.mul(10 ** uint256(_decimals));
+
+        _totalSupply = _totalSupply.sub(value);
+        _balances[msg.sender] = _balances[msg.sender].sub(value);
+        emit Transfer(msg.sender, address(0), value);
+        emit Burn(msg.sender, value);
+    }
+
+    /**
+     * @dev Function that burns an amount of the token of a given
+     * account, deducting from the sender's allowance for said account. Uses the
+     * internal burn function.
+     * Emits an Approval event (reflecting the reduced allowance).
+     * @param account The account whose tokens will be burnt.
+     * @param value The amount that will be burnt.
+     */
+    function burnFrom(address account, uint256 value) public whenNotPaused notBlacklisted(account) onlyOwner {
+        require(account != address(0));
+
+        value = value.mul(10 ** uint256(_decimals));
+        _totalSupply = _totalSupply.sub(value);
+        _balances[account] = _balances[account].sub(value);
+        emit Transfer(account, address(0), value);
+        emit Burn(account, value);
+
+        _approve(account, msg.sender, _allowed[account][msg.sender].sub(value));
+    }
+
+    /**
+     * @dev Approve an address to spend another addresses' tokens.
+     * @param owner The address that owns the tokens.
+     * @param spender The address that will spend the tokens.
+     * @param value The number of tokens that can be spent.
+     */
+    function _approve(address owner, address spender, uint256 value) internal {
+        require(spender != address(0));
+        require(owner != address(0));
+
+        _allowed[owner][spender] = value;
+        emit Approval(owner, spender, value);
+    }
+
+    function setCollectorContract(address contractAddress) onlyOwner external {
+        RupiahFeeCollector = IFeeCollector(contractAddress);
+    }
+
+    function _transfer(address from, address to, uint256 value) internal {
+        require(to != address(0));
+
+        _balances[from] = _balances[from].sub(value);
+        _balances[to] = _balances[to].add(value);
+        emit Transfer(from, to, value);
+    }
+
+    function _transferWithFee(address from, address to, uint256 value, address bridge) internal {
+        IFeeCollector feeCollector = IFeeCollector(bridge);
+
+        uint256 fee = feeCollector.calcFee(from, to, value);
+        emit TransferWithFee(from, to, value, bridge, fee);
+
+        _transfer(from, bridge, fee);
+        _transfer(from, to, value);
+    }
+}
